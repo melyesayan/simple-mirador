4d85283a5415d1f34a3eff1f8f3710f5be245f06
jpadfield-test <80904013+jpadfield-test@users.noreply.github.com>
2021-03-18 21:02:36 +0000

    Update mirador.php

--- build/extensions/mirador.php
+++ build/extensions/mirador.php
@@ -30,7 +30,10 @@ function extensionMirador ($d, $pd)
 				{$mans = listToManifest ($dets);
 				 $wo = '[{
 					"manifestId": "'.$dets[0].'"
-					}]';}
+					}]';
+				 $cats = "\"catalog\": [{\"manifestId\": \"".implode("\"}, {\"manifestId\": \"", $dets)."\"}],";}
+				else
+					{$cats = "";}
       }
     else {
 			// Used to display the JSON used to create a given page for demos
@@ -66,6 +69,7 @@ var myMiradorInstance = Mirador.viewer({
        id: "mirador",
        windows: $wo,
        manifests: $mans,
+       $cats
        $workspace
        });     
      });
